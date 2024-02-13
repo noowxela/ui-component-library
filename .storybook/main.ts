1622c0c81c659b44688073b7a82e21dc0d39f377
Alex Woon Jun ROng <noowxela@gmail.com>
2024-02-14 00:12:41 +0800

    add preset-scss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
         '@storybook/addon-essentials',
         '@storybook/addon-onboarding',
         '@storybook/addon-interactions',
-        '@storybook/addon-styling-webpack'
+        '@storybook/preset-scss',
     ],
     framework: {
         name: '@storybook/react-webpack5',
